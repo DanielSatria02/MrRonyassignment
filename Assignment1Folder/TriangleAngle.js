42fd86a0fe82abce53fcd7abbe759358ad352194
Daniel Satria <danielbussiness2000@gmail.com>
2025-03-06 23:43:13 +0700

    Update TriangleAngle.js

--- Assignment1Folder/TriangleAngle.js
+++ Assignment1Folder/TriangleAngle.js
@@ -1,1 +1,12 @@
+//this is the intro using a simple console.log to create an output that cannot be interacted with by user
+console.log("Hello, this is a calculator to calculate the missing angel for a triangle\n");
+//setting variables with value except one
+let a = 80, b = 65, c;
+//displayng the values of a & b
+console.log("angle a is: ",a, "\n");
+console.log("angle b is: ",b, "\n");
 
+c = 180 - (a + b);
+
+//placing the variables after their respective output to show result
+console.log("The missingle angel is: ", c);
