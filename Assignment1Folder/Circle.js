68e5b7d9d0410272537dba6efd1fe4e4a16922b8
Daniel Satria <danielbussiness2000@gmail.com>
2025-03-06 22:20:13 +0700

    Create Circle.js

---
+++ Assignment1Folder/Circle.js
@@ -0,0 +1,10 @@
+//this is the intro using a simple console.log to create an output that cannot be interacted with by user
+console.log("Hello, this is a calculator to calculate all possible info about a circle with a given radius\n");
+//setting one variable with a value except a few because those variables will hold the result, also let will allow one to set something as a variable
+let radius = 5, circumference;
+//displayng the values of width and length
+console.log("radius is: ",radius, "\n");
+//calculating Area, multiplying symbol is * alongside this math.pi is to import pi into the program
+circumference = 2 * Math.PI * radius;
+//placing the variables after their respective output to show result
+console.log("The circumference is: ", circumference);
