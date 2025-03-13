cf052dad66017d678c870e39a25a9334451d10aa
Daniel Satria <danielbussiness2000@gmail.com>
2025-03-14 04:17:28 +0700

    Update Circle.ts

--- WebClientDevelopmentfolder/TypeScript1/Circle.ts
+++ WebClientDevelopmentfolder/TypeScript1/Circle.ts
@@ -1,1 +1,10 @@
+// Function to calculate the area of a circle given its radius
+function CircleArea(radius: number): number {
+    // Using the Math.PI for importing pi
+    return Math.PI * radius**2;
+}
 
+// using a constant to create a variable that will hold the number for calculations
+const circleRadius: number = 5;
+// the dollar symbol is for intorpolation purposes
+console.log(`The area of a circle with radius ${number} is: ${CircleArea(number).toFixed(2)}`);
