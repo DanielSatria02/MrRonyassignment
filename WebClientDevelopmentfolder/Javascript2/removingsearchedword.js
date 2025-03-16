5ff270cc89277efc496aa03edc6110a25c98e9ba
Daniel Satria <danielbussiness2000@gmail.com>
2025-03-16 09:33:49 +0700

    Update removingsearchedword.js

--- WebClientDevelopmentfolder/Javascript2/removingsearchedword.js
+++ WebClientDevelopmentfolder/Javascript2/removingsearchedword.js
@@ -1,1 +1,11 @@
+function remove_first_occurrence(str, searchstr)       {
+	var index = str.indexOf(searchstr);
+	if (index === -1) {
+		return str;
+	}
+	return str.slice(0, index) + str.slice(index + searchstr.length);
+}
 
+let belremover = prompt("Enter a word that has bel: ");
+
+console.log(remove_first_occurrence(belremover, 'bel'));
